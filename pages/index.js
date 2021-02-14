16a75481eec395ca905fdabe64111cba1dd5b875
Emad Al-Futahi <eakram26@gmail.com>
2021-02-15 00:15:02 +0300

    nextPwa

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,20 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+
+    
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+
+<link rel="manifest" href="/manifest.json" />
+
+<meta name="theme-color" content="#90cdf4" />
       </Head>
+
+        
+      
 
       <main className={styles.main}>
         <h1 className={styles.title}>
